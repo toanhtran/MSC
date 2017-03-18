ab60391ed957e333eff335f2b5062027b627a8c6
toanhtran <toanh.t.tran@gmail.com>
2017-03-18 12:05:25 -0700

    sub navbar

--- wp-content/themes/msc-mdb/header.php
+++ wp-content/themes/msc-mdb/header.php
@@ -12,5 +12,40 @@
     </title>
     <?php wp_head(); ?>
 </head> 
-<body>               
+<body> 
+
+<header>
+    <!--Navbar-->
+    <nav class="navbar navbar-dark primary-color-dark">
+        <!-- Collapse button-->
+        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#collapseEx">
+            <i class="fa fa-bars"></i>
+        </button>
+        <div class="container">
+            <!--Collapse content-->
+            <div class="collapse navbar-toggleable-xs" id="collapseEx">
+                <!--Navbar Brand-->
+                <a class="navbar-brand" href="https://mdbootstrap.com/material-design-for-bootstrap/" target="_blank">MDB</a>
+                <!--Links-->
+                <ul class="nav navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Resources</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!--/.Collapse content-->
+        </div>
+    </nav>
+    <!--/.Navbar-->
+</header>
+                          
                 
