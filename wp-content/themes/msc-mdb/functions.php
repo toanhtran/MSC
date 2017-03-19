63d72341f5a903e24497686cc2250d1a146206f6
toanhtran <toanh.t.tran@gmail.com>
2017-03-18 18:05:50 -0700

    sidebar

--- wp-content/themes/msc-mdb/functions.php
+++ wp-content/themes/msc-mdb/functions.php
@@ -36,6 +36,22 @@ function MDB_setup() {
   }
   add_action('after_setup_theme', 'MDB_setup');
 
+/**
+ * Register our sidebars and widgetized areas.
+ */
+function mdb_widgets_init() {
 
+  register_sidebar( array(
+    'name'          => 'Sidebar',
+    'id'            => 'sidebar',
+    'before_widget' => '',
+    'after_widget'  => '',
+    'before_title'  => '',
+    'after_title'   => '',
+  ) );
+
+}
+add_action( 'widgets_init', 'mdb_widgets_init' );
+            
 ?>
             
--- wp-content/themes/msc-mdb/index.php
+++ wp-content/themes/msc-mdb/index.php
@@ -83,10 +83,15 @@
             } // end if
             ?>
         </div>
+
         <!--Sidebar-->
         <div class="col-md-4">
+            <?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+            <?php dynamic_sidebar( 'sidebar' ); ?>
+            <?php endif; ?>
         </div>
-        <!--/.Sidebar-->
+        <!--/.Sidebar-->            
+            
     </div>
 </div>
 <!--/.Main layout-->
